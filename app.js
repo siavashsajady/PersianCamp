b0ea21c364d0f32e69189f7878aa0cf3da430869
Siavash <siavash.sajjady@gmail.com>
2019-02-17 12:17:03 +0000

    add mongolab

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-mongoose.connect('mongodb://localhost:27017/yelp_camp_v17', { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost:27017/yelp_camp_v17', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://oscar:salam@persiancamp-5rpnm.mongodb.net/test?retryWrites=true');
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
